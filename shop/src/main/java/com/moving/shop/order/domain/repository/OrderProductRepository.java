69eb136962a0a3ca0c0334e35b3d599ddb1f27db
kimsolee95 <ahhasolee@naver.com>
2023-02-28 13:05:03 +0900

    feat: [010-CUSTOMER-ORDER] 주문data생성 서비스 test code 추가 및 주문상품(옵션 포함 ver) 조회 기능에 필요한 jpa 라인 추가

--- shop/src/main/java/com/moving/shop/order/domain/repository/OrderProductRepository.java
+++ shop/src/main/java/com/moving/shop/order/domain/repository/OrderProductRepository.java
@@ -1,8 +1,14 @@
 package com.moving.shop.order.domain.repository;
 
 import com.moving.shop.order.domain.entity.OrderProduct;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
 
+  @EntityGraph(attributePaths = {"orderProductOptions"}, type = EntityGraphType.LOAD)
+  Optional<OrderProduct> findWithOrderProductOptionByServiceOrder_Id(@RequestParam("service_order_id") Long serviceOrderId);
 }
