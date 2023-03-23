a54ab90db3e0cf3ed397d2fe93f4fbd203c822f1
kimsolee95 <ahhasolee@naver.com>
2023-03-23 23:46:55 +0900

    feat : [013-SWAGGER] customer package 내 컨트롤러 스웨거 주석 추가

--- shop/src/main/java/com/moving/shop/customer/controller/CustomerSignUpController.java
+++ shop/src/main/java/com/moving/shop/customer/controller/CustomerSignUpController.java
@@ -2,6 +2,7 @@ package com.moving.shop.customer.controller;
 
 import com.moving.shop.customer.application.CustomerSignUpApplication;
 import com.moving.shop.customer.domain.dto.SignUpForm;
+import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class CustomerSignUpController {
   private final CustomerSignUpApplication customerSignUpApplication;
 
   @PostMapping("/customer")
+  @ApiOperation(value="고객 회원가입 API", notes = "고객 회원 유형으로 회원가입을 합니다.")
   public ResponseEntity<String> customerSignUp(@Valid @RequestBody SignUpForm form) {
     return ResponseEntity.ok(customerSignUpApplication.customerSignUp(form));
   }
